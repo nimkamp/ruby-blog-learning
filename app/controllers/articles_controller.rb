f0662c931507173a670726898ff722969c68846b
Nick Imkamp <imkampn@gmail.com>
2019-02-24 10:06:17 -0800

    Whitelist controller parameters for creating article. Add ability to save article. Add function to show article once created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,21 @@
 class ArticlesController < ApplicationController
+
+  def create
+    @article = Article.new(article_params)
+    
+    @article.save
+    redirect_to @article
+  end
+
   def new
   end
-  def create
-    render plain: params[:article].inspect
+
+  def show
+    @article = Article.find(params[:id])
   end
+
+  private
+     def article_params
+      params.require(:article).permit(:title, :text)
+     end
 end
