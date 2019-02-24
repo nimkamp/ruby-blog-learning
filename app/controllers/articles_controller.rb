9b96749edf85eb80223b264811c5d9c3289ed86f
Nick Imkamp <imkampn@gmail.com>
2019-02-24 14:29:09 -0800

    Create actions in controller to create a new instance of an article, update article, and list all the articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,32 @@
 class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def update
+    @article = Article.find(params[:id])
+  end
 
   def create
     @article = Article.new(article_params)
     
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
-
-  def new
+  
+  def index
+    @article = Article.all
   end
-
+  
   def show
     @article = Article.find(params[:id])
   end
-
+  
   private
-     def article_params
-      params.require(:article).permit(:title, :text)
-     end
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
